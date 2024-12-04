9850b8ea36f0f101a7534b61e1bfab27e9d0ce7d
Speer <gustav01.noren@gmail.com>
2024-12-04 07:37:25 +0100

    better

--- solutions/day4.py
+++ solutions/day4.py
@@ -8,7 +8,7 @@ lines = uf.read_lines("../input/4.in")
 N = len(lines)
 M = len(lines[0])
 
-def search(x, y, dire):
+def findXMAS(x, y, dire):
     dx, dy = dire
     s = ""
     for i in range(4):
@@ -24,44 +24,26 @@ def solve():
     for x in range(N):
         for y in range(M):
             for direction in itertools.product([-1, 0, 1], repeat=2):
-                if search(x, y, direction):
+                if findXMAS(x, y, direction):
                     first += 1
-            if search3x3(x, y):
-                print(x,y)
+            if findMAS(x, y):
                 second += 1
                 
     return (first, second)
 
 
-def search3x3(x, y):
-    if not (1 <= x < N-1 and 1 <= y < M-1):
-        return False
-    
+def findMAS(x, y):
     count = 0
-    s = ""
-    s += lines[x-1][y-1]
-    s += lines[x][y]
-    s += lines[x+1][y+1]
-    if s == "MAS":
-        count += 1
-    s = ""
-    s += lines[x-1][y+1]
-    s += lines[x][y]
-    s += lines[x+1][y-1]
-    if s == "MAS":
-        count += 1
-    s = ""
-    s += lines[x+1][y+1]
-    s += lines[x][y]
-    s += lines[x-1][y-1]
-    if s == "MAS":
-        count += 1
-    s = ""
-    s += lines[x+1][y-1]
-    s += lines[x][y]
-    s += lines[x-1][y+1]
-    if s == "MAS":
-        count += 1
+    for i,j in itertools.product([-1,1], repeat=2):
+        dx, dy = -i, -j
+        s = ""
+        for k in range(3):
+            nx, ny = x + i + dx * k, y + j + dy * k
+            if not (0 <= nx < N and 0 <= ny < M):
+                return False
+            s += lines[nx][ny]
+        if s == "MAS":
+            count += 1
     return count >= 2
     
 
